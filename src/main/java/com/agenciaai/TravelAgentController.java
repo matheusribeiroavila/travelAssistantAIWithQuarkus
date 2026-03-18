ad35c8e6b4567e93c3466542ce3c35e0b54eb833
Matheus Ribeiro Ávila <ribeiroavilamatheus@gmail.com>
2026-03-18 07:50:28 -0300

    ADD the EasyRag as extension, also created the controller to consume our POST REQ

---
+++ src/main/java/com/agenciaai/TravelAgentController.java
@@ -0,0 +1,24 @@
+package com.agenciaai;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.awt.*;
+
+@Path("/travel")
+public class TravelAgentController {
+
+    @Inject
+    TravelAgentAssistant assistant;
+
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String ask(String question){
+        return assistant.chat(question);
+    }
+}
